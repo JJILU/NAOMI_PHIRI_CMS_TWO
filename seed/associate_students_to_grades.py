9af2d3ffda24111d88646dfea786c11ed1532773
naomixox <walu9963@gmail.com>
2025-12-02 17:02:36 +0200

    created student grade half

--- seed/associate_students_to_grades.py
+++ seed/associate_students_to_grades.py
@@ -9,36 +9,29 @@ app = create_app()
 
 
 with app.app_context():
-    # get all students 
     all_students = StudentSchoolRecord.query.all()
-    print(all_students)
 
-    try:
-        for student in all_students:
-            # get class subjects
-            student_classroom = Classroom.query.get(student.id)
-            print(student_classroom)
+    for student in all_students:
 
+        # the correct classroom
+        student_classroom = student.classroom  
 
-            # is_present = choices([True,False],weights=[50,50])[0]
-            # new_attendance = StudentGrade(
-            #     exam_code=,
-            #     exam_subject_name=,
-            #     student_grade=,
-            #     student_score=,
-            #     student_school_record_id=student.id
-            # )
+        if not student_classroom:
+            print("Student has no classroom. Skipping…")
+            continue
 
-            # db.session.add(new_attendance)
+        cs_list = student_classroom.compulsary_subjects
+        op_list = student_classroom.optional_subjects
 
-        # db.session.commit()  
-    except Exception as e:
-        db.session.rollback()
-        print(f"failed to create student attendances : {str(e)}")
+        # skip empty lists
+        if not cs_list or not op_list:
+            print("Subjects missing. Skipping...")
+            continue
 
+        com_weights = [1] * len(cs_list)
+        op_weights = [1] * len(op_list)
 
-    
+        com_subject = choices(cs_list, weights=com_weights, k=1)[0]
+        optional = choices(op_list, weights=op_weights, k=1)[0]
 
-        
-
-
+        print(com_subject, optional)
